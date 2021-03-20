1e604732175529854c97895f32438809903ffc1d
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-20 20:54:35 +1000

    Experiment with cpp style strings

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 typedef float metres;
 
@@ -15,13 +16,13 @@ enum daysOfTheWeek {
 void print_height(metres height);
 void iterate_days_of_the_week(daysOfTheWeek days);
 void sum_two_numbers(int num1, int num2 = 100);
+void add_two_strings(std::string string1, std::string string2);
 
 int main(void) {
 
-    int num1 = 10;
-    int num2 = 10;
-    sum_two_numbers(10);
-    sum_two_numbers(10, 10);
+    std::string string1 = "Hello ";
+    std::string string2 = "World";
+    add_two_strings(string1, string2);
 
     return 0;
 }
@@ -36,4 +37,9 @@ void iterate_days_of_the_week(daysOfTheWeek day) {
 
 void sum_two_numbers(int num1, int num2/*Default value = 100*/) {
     std::cout << "The sum is: " << num1 + num2 << '\n';
+}
+
+void add_two_strings(std::string string1, std::string string2) {
+    std::cout << "Addition of two strings is: " << string1 + string2 << '\n';
+    std::cout << "Length of combined string is: " << (string1 + string2).length() << '\n';
 }

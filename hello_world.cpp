4a2f5a26573ee955a08ad296c3da5b3e61e8d796
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-20 20:30:13 +1000

    Experiment with enums

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,16 +2,35 @@
 
 typedef float metres;
 
+enum daysOfTheWeek {
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
+    sunday
+};
+
 void print_height(metres height);
+void iterate_days_of_the_week(daysOfTheWeek days);
 
 int main(void) {
     std::cout << "Hello world!\n";
 
-    print_height(100.75);
+    metres height = 100.75;
+    print_height(height);
+
+    daysOfTheWeek day = tuesday;
+    iterate_days_of_the_week(day);
 
     return 0;
 }
 
 void print_height(metres height) {
     std::cout << "Height is: " << height << "\n";
+}
+
+void iterate_days_of_the_week(daysOfTheWeek day) {
+    std::cout << "The day of the week is number: " << day << "\n";
 }

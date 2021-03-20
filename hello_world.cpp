d8718ae662d1add2c3bbcc3c86fdae100c8184b1
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-20 20:22:22 +1000

    Experiment with typedefs

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+typedef float metres;
+
+void print_height(metres height);
+
 int main(void) {
     std::cout << "Hello world!\n";
+
+    print_height(100.75);
+
     return 0;
+}
+
+void print_height(metres height) {
+    std::cout << "Height is: " << height << "\n";
 }

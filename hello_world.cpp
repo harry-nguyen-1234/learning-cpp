b385c1dd66a69ce013723c0130576195e611ffc2
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-20 20:41:58 +1000

    Experiment with default arguments

--- hello_world.cpp
+++ hello_world.cpp
@@ -14,23 +14,26 @@ enum daysOfTheWeek {
 
 void print_height(metres height);
 void iterate_days_of_the_week(daysOfTheWeek days);
+void sum_two_numbers(int num1, int num2 = 100);
 
 int main(void) {
-    std::cout << "Hello world!\n";
 
-    metres height = 100.75;
-    print_height(height);
-
-    daysOfTheWeek day = tuesday;
-    iterate_days_of_the_week(day);
+    int num1 = 10;
+    int num2 = 10;
+    sum_two_numbers(10);
+    sum_two_numbers(10, 10);
 
     return 0;
 }
 
 void print_height(metres height) {
-    std::cout << "Height is: " << height << "\n";
+    std::cout << "Height is: " << height << '\n';
 }
 
 void iterate_days_of_the_week(daysOfTheWeek day) {
-    std::cout << "The day of the week is number: " << day << "\n";
+    std::cout << "The day of the week is number: " << day << '\n';
+}
+
+void sum_two_numbers(int num1, int num2/*Default value = 100*/) {
+    std::cout << "The sum is: " << num1 + num2 << '\n';
 }
